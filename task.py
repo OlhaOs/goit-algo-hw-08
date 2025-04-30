f8e266e09f9fa2f4f343519c04e9e153c9ff7077
OlhaOs <olalaadka@gmail.com>
2025-04-30 08:55:46 +0300

    fix after check

--- task.py
+++ task.py
@@ -9,20 +9,23 @@ def heap_sort(iterable):
 
 def min_cost(cables):
     if not cables:
-        cost = 0
-    elif len(cables) < 2:
-        cost = cables[0]
+        return 0
+    
+    elif len(cables) == 1:
+        return 0
+    
     else:
         cabl_sorted = heap_sort(cables)
         print(cabl_sorted)
         cost = cabl_sorted[0] + cabl_sorted[1]
         new_cables = cabl_sorted[2:]
         new_cables.append(cost)
-        cost =+ min_cost(new_cables)
-    return cost
+        return cost + min_cost(new_cables)
+    
 
 if __name__ == "__main__":
     cables = [15,3,4,10,12,8]
    
 
     print(min_cost(cables))
+
